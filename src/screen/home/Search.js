4ff4f4af31b0f6d9c6423a0905ff9a6f00d544ce
votadung <dung.vo@vtl-vtl.com>
2023-09-27 13:16:07 +0700

    add file config firebase.json

--- src/screen/home/Search.js
+++ src/screen/home/Search.js
@@ -14,6 +14,7 @@ import {debounce} from 'lodash';
 import {FlatList} from 'react-native-gesture-handler';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Entypo from 'react-native-vector-icons/Entypo';
+
 import {ApiSearchWeather} from '../../actions/Api';
 
 const {width} = Dimensions.get('screen');
